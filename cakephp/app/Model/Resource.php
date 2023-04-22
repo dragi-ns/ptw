fb5717178f6fa9f4b370c10c05da5ee5f1df19c5
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2023-04-22 20:37:41 +0200

    Fix the admin resource filtering

--- cakephp/app/Model/Resource.php
+++ cakephp/app/Model/Resource.php
@@ -76,4 +76,34 @@ class Resource extends AppModel {
 			'message' => 'Please select at least one category.'
 		)
 	);
+
+	public function getCount($conditions = array()) {
+		$settings = array(
+			'conditions' => $conditions,
+			'recursive' => 0
+		);
+
+		if (array_key_exists('Category.id', $conditions)) {
+			$settings['joins'][] = array(
+				'table' => 'categories_resources',
+				'alias' => 'CategoriesResources',
+				'type' => 'inner',
+				'conditions' => array(
+					'CategoriesResources.resource_id = Resource.id',
+					'CategoriesResources.category_id' => $conditions['Category.id']
+				)
+			);
+
+			$settings['joins'][] = array(
+				'table' => 'categories',
+				'alias' => 'Category',
+				'type' => 'inner',
+				'conditions' => array(
+					'CategoriesResources.category_id = Category.id'
+				)
+			);
+		}
+
+		return $this->find('count', $settings);
+	}
 }
